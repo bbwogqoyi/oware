7aae1172dbd264f8b2ed2433571fcbe6c2dcc93e
Thabo <thabosekgotho2@gmail.com>
2020-05-17 03:18:35 +0200

    I have added the getSeeds method. Please check it out and let me know

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -16,7 +16,43 @@ score: (int*int)
 gamestatus: state
 }
 
-let getSeeds n board = failwith "Not implemented"
+(* The following function is intended to retrieve information about the number
+   of seeds present within a given house, using a house number and the playing 
+   field
+   
+   I will make use of the BoardState's board label defined in line 14 as the 
+   playing field and bind the return integer value to an idenifying that will take
+   the place of the given number of seeds in that house at that particular time.
+
+   To achieve this, I will use a match expression with twelve patterns representing
+   the game's twelve houses. To keep things simple, I will then bind the return value
+   to an identifier starting from one through twelve (e.g. num1, num2, etc.).
+
+   I had an error in referencing our board via BoardState.board - intelliSense flagged
+   'board' as non static and since its not a literal, I couldn't use it in a pattern
+   match. I then assigned it to a temporary identifier called playingMedium for the use
+   in the pattern match. I then defined the return type as an integer. I am also taking
+   cue from Siyanda's interface, labeling my houses like he did. 
+
+   There aren't any tests pertaining to this function in its solitude. I hope it works.
+
+   Please don't hesitate to give me any feedback and we can implement those changes.
+ *)
+let getSeeds houseNumber {BoardState.board = playingMedium} : int = 
+ match houseNumber, playingMedium with
+ | 1, (num1,_,_,_,_,_,_,_,_,_,_,_) -> num1
+ | 2, (_,num2,_,_,_,_,_,_,_,_,_,_) -> num2
+ | 3, (_,_,num3,_,_,_,_,_,_,_,_,_) -> num3
+ | 4, (_,_,_,num4,_,_,_,_,_,_,_,_) -> num4
+ | 5, (_,_,_,_,num5,_,_,_,_,_,_,_) -> num5
+ | 6, (_,_,_,_,_,num6,_,_,_,_,_,_) -> num6
+ | 7, (_,_,_,_,_,_,num7,_,_,_,_,_) -> num7
+ | 8, (_,_,_,_,_,_,_,num8,_,_,_,_) -> num8
+ | 9, (_,_,_,_,_,_,_,_,num9,_,_,_) -> num9
+ | 10, (_,_,_,_,_,_,_,_,_,num10,_,_) -> num10
+ | 11, (_,_,_,_,_,_,_,_,_,_,num11,_) -> num11
+ | 12, (_,_,_,_,_,_,_,_,_,_,_,num12) -> num12
+ | _ -> failwith "Not implemented"
 
 let useHouse n board = failwith "Not implemented"
 
